4ef99e21327e9fa036ec4aeefd1e1e0d8160c958
Rowan Stein <rowan.stein@agyn.io>
2026-02-24 03:18:10 +0000

    feat(system+ci): add Istio/Argo CD system stack and CI (#4)
    * feat(system+ci): add Istio/Argo CD system stack and CI to apply stacks in order
    
    * fix(stacks): restore kubeconfig and locks
    
    * fix(ci): use default kubeconfig var
    
    ---------
    
    Co-authored-by: Casey Brooks <casey.brooks@agyn.io>

---
+++ stacks/system/outputs.tf
@@ -0,0 +1,18 @@
+output "installed_namespaces" {
+  value = [
+    kubernetes_namespace.istio_system.metadata[0].name,
+    kubernetes_namespace.istio_gateway.metadata[0].name,
+    kubernetes_namespace.argocd.metadata[0].name,
+  ]
+  description = "Installed namespaces"
+}
+
+output "releases" {
+  value = [
+    helm_release.istio_base.name,
+    helm_release.istiod.name,
+    helm_release.istio_gateway.name,
+    helm_release.argo_cd.name,
+  ]
+  description = "Installed Helm releases"
+}
---
+++ stacks/system/variables.tf
@@ -0,0 +1,17 @@
+variable "kubeconfig_path" {
+  type        = string
+  description = "Path to kubeconfig for connecting to the cluster"
+  default     = "../k8s/.kube/agyn-local-kubeconfig.yaml"
+}
+
+variable "istio_chart_version" {
+  type        = string
+  description = "Istio chart version"
+  default     = "1.21.0"
+}
+
+variable "argocd_chart_version" {
+  type        = string
+  description = "Argo CD chart version"
+  default     = "5.33.0"
+}
