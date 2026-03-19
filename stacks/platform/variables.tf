8531b23aa6649f28ce5277f37e952b56d363a214
Rowan Stein <rowan.stein@agyn.io>
2026-03-19 03:53:58 +0000

    chore: bump orchestrator to 0.1.5, add DOCKER_RUNNER_SHARED_SECRET

--- stacks/platform/variables.tf
+++ stacks/platform/variables.tf
@@ -44,7 +44,7 @@ variable "agent_state_chart_version" {
 variable "agents_orchestrator_chart_version" {
   type        = string
   description = "Version of the agents-orchestrator Helm chart published to GHCR"
-  default     = "0.1.4"
+  default     = "0.1.5"
 }
 
 variable "threads_chart_version" {
