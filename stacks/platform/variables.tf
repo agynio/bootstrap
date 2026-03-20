b93a117051b5b92a5ad3ffe9bb02ce3b0f3a3c4f
Rowan Stein <rowan.stein@agyn.io>
2026-03-20 23:27:18 +0000

    chore: bump chat-app to v0.1.5 (fix nginx config alias)

--- stacks/platform/variables.tf
+++ stacks/platform/variables.tf
@@ -92,7 +92,7 @@ variable "tenants_chart_version" {
 variable "chat_app_chart_version" {
   type        = string
   description = "Version of the chat-app Helm chart published to GHCR"
-  default     = "0.1.4"
+  default     = "0.1.5"
 }
 
 variable "chat_app_image_tag" {
