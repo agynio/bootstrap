32123d877c6856ba5b8b09ec77df0b94bd143dce
Rowan Stein <rowan.stein@agyn.io>
2026-03-21 01:16:30 +0000

    chore: bump chat-app to v0.1.6 (static OIDC metadata)

--- stacks/platform/variables.tf
+++ stacks/platform/variables.tf
@@ -92,7 +92,7 @@ variable "tenants_chart_version" {
 variable "chat_app_chart_version" {
   type        = string
   description = "Version of the chat-app Helm chart published to GHCR"
-  default     = "0.1.5"
+  default     = "0.1.6"
 }
 
 variable "chat_app_image_tag" {
