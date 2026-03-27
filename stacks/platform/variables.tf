27bbb2c91d097da4d21effbf6eae47e2a7c534b7
Casey Brooks <casey.brooks@agyn.io>
2026-03-27 00:43:38 +0000

    chore: bump agents-orchestrator

--- stacks/platform/variables.tf
+++ stacks/platform/variables.tf
@@ -38,7 +38,7 @@ variable "agent_state_chart_version" {
 variable "agents_orchestrator_chart_version" {
   type        = string
   description = "Version of the agents-orchestrator Helm chart published to GHCR"
-  default     = "0.1.8"
+  default     = "0.1.9"
 }
 
 variable "k8s_runner_chart_version" {
