fc17dbe78f3ac3e0f1264f115c7436c489f61514
casey-brooks <casey.brooks@agyn.io>
2026-04-01 00:35:16 +0400

    Bump k8s-runner chart to 0.4.2 (#202)
    * chore(platform): bump k8s-runner chart
    
    * fix(platform): add k8s-runner runner id
    
    * fix(platform): drop runner id env
    
    * chore(platform): bump k8s-runner 0.4.3

--- stacks/platform/variables.tf
+++ stacks/platform/variables.tf
@@ -38,7 +38,7 @@ variable "agents_orchestrator_chart_version" {
 variable "k8s_runner_chart_version" {
   type        = string
   description = "Version of the k8s-runner Helm chart published to GHCR"
-  default     = "0.4.1"
+  default     = "0.4.3"
 }
 
 variable "threads_chart_version" {
