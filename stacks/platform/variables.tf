c7cbbd837e58e05ba7ac5c4c19e79d7d04ba6188
Rowan Stein <rowan.stein@agyn.io>
2026-04-11 21:46:20 +0000

    chore: bump ziti-management chart to 0.10.0

--- stacks/platform/variables.tf
+++ stacks/platform/variables.tf
@@ -93,7 +93,7 @@ variable "agents_chart_version" {
 variable "ziti_management_chart_version" {
   type        = string
   description = "Version of the ziti-management Helm chart published to GHCR"
-  default     = "0.9.0"
+  default     = "0.10.0"
 }
 
 variable "users_chart_version" {
