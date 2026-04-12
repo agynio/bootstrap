4e1e9dd76a6dbb68b494c0aafc41218682e7737a
casey-brooks <casey.brooks@agyn.io>
2026-04-12 07:43:38 +0400

    chore(platform): bump chart versions (#298)
    * chore(platform): bump chart versions
    
    * chore(platform): bump ziti chart

--- stacks/platform/variables.tf
+++ stacks/platform/variables.tf
@@ -93,13 +93,13 @@ variable "agents_chart_version" {
 variable "ziti_management_chart_version" {
   type        = string
   description = "Version of the ziti-management Helm chart published to GHCR"
-  default     = "0.10.0"
+  default     = "0.10.2"
 }
 
 variable "users_chart_version" {
   type        = string
   description = "Version of the users Helm chart published to GHCR"
-  default     = "0.5.0"
+  default     = "0.5.1"
 }
 
 variable "expose_chart_version" {
