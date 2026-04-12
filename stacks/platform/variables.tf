2ace9f53d270498d7b7e2311e24c367d1473c07d
Rowan Stein <rowan.stein@agyn.io>
2026-04-12 12:36:19 +0000

    bump chat-app to v0.4.3

--- stacks/platform/variables.tf
+++ stacks/platform/variables.tf
@@ -135,7 +135,7 @@ variable "apps_chart_version" {
 variable "chat_app_chart_version" {
   type        = string
   description = "Version of the chat-app Helm chart published to GHCR"
-  default     = "0.4.2"
+  default     = "0.4.3"
 }
 
 variable "chat_app_image_tag" {
