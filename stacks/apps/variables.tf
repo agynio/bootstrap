c1a24d051fd01b5d6e1fd582a4187645a1202e31
Casey Brooks <casey.brooks@agyn.io>
2026-04-13 19:46:21 +0000

    chore(stacks): bump chart defaults

--- stacks/apps/variables.tf
+++ stacks/apps/variables.tf
@@ -50,7 +50,7 @@ variable "reminders_image_tag" {
 variable "k8s_runner_chart_version" {
   type        = string
   description = "Version of the k8s-runner Helm chart published to GHCR"
-  default     = "0.10.2"
+  default     = "0.10.3"
 }
 
 variable "k8s_runner_image_tag" {
--- stacks/platform/variables.tf
+++ stacks/platform/variables.tf
@@ -724,7 +724,7 @@ variable "media_proxy_image_tag" {
 variable "llm_proxy_chart_version" {
   type        = string
   description = "Version of the llm-proxy Helm chart published to GHCR"
-  default     = "0.10.2"
+  default     = "0.10.3"
 }
 
 variable "llm_proxy_image_tag" {
