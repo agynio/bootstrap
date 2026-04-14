9a8521f70d84c985e782c57943af1664a9b400a6
casey-brooks <casey.brooks@agyn.io>
2026-04-14 15:22:47 +0400

    chore(platform): bump threads/runners charts (#323)
    * chore(platform): bump chart versions
    
    * chore(platform): update threads defaults

--- stacks/platform/variables.tf
+++ stacks/platform/variables.tf
@@ -45,7 +45,7 @@ variable "agents_orchestrator_chart_version" {
 variable "threads_chart_version" {
   type        = string
   description = "Version of the threads Helm chart published to GHCR"
-  default     = "0.3.2"
+  default     = "0.4.2"
 }
 
 variable "metering_chart_version" {
@@ -123,7 +123,7 @@ variable "identity_chart_version" {
 variable "runners_chart_version" {
   type        = string
   description = "Version of the runners Helm chart published to GHCR"
-  default     = "0.5.0"
+  default     = "0.5.1"
 }
 
 variable "apps_chart_version" {
