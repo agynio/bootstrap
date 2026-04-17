d7670534c2e638711e8414fdd32fa3935b781a6b
casey-brooks <casey.brooks@agyn.io>
2026-04-17 20:51:24 +0400

    chore(platform): bump console app chart (#364)

--- stacks/platform/variables.tf
+++ stacks/platform/variables.tf
@@ -147,7 +147,7 @@ variable "chat_app_image_tag" {
 variable "console_app_chart_version" {
   type        = string
   description = "Version of the console-app Helm chart published to GHCR"
-  default     = "0.9.0"
+  default     = "0.9.1"
 }
 
 variable "console_app_image_tag" {
