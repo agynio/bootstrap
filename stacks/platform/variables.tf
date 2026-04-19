60d8a330a045b2a00e77afa7153f4936eb9945e9
casey-brooks <casey.brooks@agyn.io>
2026-04-20 01:45:33 +0400

    fix(platform): bump agents chart (#383)

--- stacks/platform/variables.tf
+++ stacks/platform/variables.tf
@@ -87,7 +87,7 @@ variable "postgres_chart_version" {
 variable "agents_chart_version" {
   type        = string
   description = "Version of the agents Helm chart published to GHCR"
-  default     = "0.7.0"
+  default     = "0.7.1"
 }
 
 variable "ziti_management_chart_version" {
