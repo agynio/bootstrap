6c3ef1ff5bcc9cfe66b80aabaa33cdaca6a7ffc4
Casey Brooks <casey.brooks@agyn.io>
2026-04-24 16:29:29 +0000

    chore(platform): bump chart pins

--- stacks/platform/variables.tf
+++ stacks/platform/variables.tf
@@ -99,7 +99,7 @@ variable "ziti_management_chart_version" {
 variable "users_chart_version" {
   type        = string
   description = "Version of the users Helm chart published to GHCR"
-  default     = "0.5.1"
+  default     = "0.5.2"
 }
 
 variable "expose_chart_version" {
@@ -111,7 +111,7 @@ variable "expose_chart_version" {
 variable "organizations_chart_version" {
   type        = string
   description = "Version of the organizations Helm chart published to GHCR"
-  default     = "0.4.1"
+  default     = "0.4.2"
 }
 
 variable "identity_chart_version" {
