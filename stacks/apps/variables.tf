b8cca66abceef7892a54bb4f4688afc210190f4f
Jules Vega <jules.vega@agyn.io>
2026-05-12 04:40:18 +0000

    chore(apps): bump reminders chart to 0.2.5

--- stacks/apps/variables.tf
+++ stacks/apps/variables.tf
@@ -49,7 +49,7 @@ variable "postgres_chart_version" {
 variable "reminders_chart_version" {
   type        = string
   description = "Version of the reminders Helm chart published to GHCR"
-  default     = "0.2.4"
+  default     = "0.2.5"
 }
 
 variable "reminders_image_tag" {
