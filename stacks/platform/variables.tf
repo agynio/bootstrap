1f2efd3e41ae07e3495cd50ce833e7c139372f50
Rowan Stein <rowan.stein@agyn.io>
2026-05-13 21:57:14 +0000

    chore(platform): bump llm-proxy chart to 0.12.1

--- stacks/platform/variables.tf
+++ stacks/platform/variables.tf
@@ -659,7 +659,7 @@ variable "media_proxy_image_tag" {
 variable "llm_proxy_chart_version" {
   type        = string
   description = "Version of the llm-proxy Helm chart published to GHCR"
-  default     = "0.12.0"
+  default     = "0.12.1"
 }
 
 variable "llm_proxy_image_tag" {
