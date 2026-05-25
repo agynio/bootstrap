e8b2e6a032f6289f5ef283d215f0d88ccafcf04c
Rowan Stein <rowan.stein@agyn.io>
2026-05-25 08:58:16 +0400

    Bump threads chart version to 0.4.13 (#527)

--- stacks/platform/variables.tf
+++ stacks/platform/variables.tf
@@ -45,7 +45,7 @@ variable "agents_orchestrator_chart_version" {
 variable "threads_chart_version" {
   type        = string
   description = "Version of the threads Helm chart published to GHCR"
-  default     = "0.4.11"
+  default     = "0.4.13"
 }
 
 variable "metering_chart_version" {
